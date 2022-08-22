259e8231ac687ee4974606148bc997c05d0dd6d6
Ahmed AL Awamleh <awamlehad@icloud.com>
2022-08-22 03:15:22 +0300

    final result

---
+++ src/components/header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import NavPar from "./navpar";
+
+class Header extends React.Component {
+    render() {
+        return (
+            <>
+                <NavPar />
+
+            </>
+        )
+    }
+}
+
+export default Header;
+
+
+
---
+++ src/components/horned.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Main from "./main";
+import Card from 'react-bootstrap/Card';
+
+class Horned extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            fav: 0
+        }
+    }
+    favClick = () => {
+        this.setState({ fav: this.state.fav + 1 });
+        this.props.passModal(this.props.data)
+    }
+
+
+    render() {
+        return (
+            <>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={this.props.img_Url} alt={this.props.title} onClick={this.favClick} />
+                    <Card.Body>
+                        <Card.Title>{this.props.title}👍{this.state.fav}</Card.Title>
+                        <Card.Text>{this.props.discription}  </Card.Text>
+
+                    </Card.Body>
+                </Card>
+
+            </>
+        )
+    }
+}
+
+export default Horned;
